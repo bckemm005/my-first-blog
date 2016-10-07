22387134c982fed7a8e31379ee3210b014834581
Your Name <you@example.com>
2016-10-07 12:39:07 +0200

    My Django Girls app, first commit

---
+++ .gitignore.py
@@ -0,0 +1,6 @@
+*.pyc
+__pycache__
+bckemm005
+db.sqlite3
+/static
+.DS_Store
---
+++ bckemm005/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/emmabuckland/bckemm005/djangogirls/bckemm005/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
